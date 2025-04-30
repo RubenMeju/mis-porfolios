a684365e79c959250e72d0d67a0147e586e8b4cc
Rubén Y <rubenmeju@outlook.es>
2025-04-30 19:43:08 +0200

    quitar minimalist

--- src/app/minimalist/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Suspense } from "react";
-
-export default function RetroLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <Suspense fallback={null}>{children}</Suspense>;
-}
